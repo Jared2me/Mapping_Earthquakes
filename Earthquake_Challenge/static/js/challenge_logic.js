3427dfc8a8ea0774fe51ee54c6ca52e6a79c753b
Jared2me <Jaredtoomey@icloud.com>
2022-11-09 20:51:34 -0500

    final

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -122,7 +122,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     // 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
   d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
 
-  // 4. Use the same style as the earthquake data.
   function styleInfo(feature) {
     return {
       opacity: 1,
@@ -135,6 +134,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     };
   }
   
+
   // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
   function getColor(magnitude) {
     if (magnitude > 6) {
@@ -146,6 +146,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     return "#eecc00";
   }
   
+
   // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
   function getRadius(magnitude) {
     if (magnitude === 0) {
@@ -153,7 +154,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
     return magnitude * 4;
   }
-
 
   // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
   // sets the style of the circle, and displays the magnitude and location of the earthquake
---
+++ README.md
@@ -0,0 +1,9 @@
+## Mapping Earthquakes
+
+## Overview
+-This project involved creating a map using MapBox using different layers to visualize earthquake intensity and proximity to tectonic plates. 
+
+## Results:
+
+![launch](Resources/Map.PNG)
+
